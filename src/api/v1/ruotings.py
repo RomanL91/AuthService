1d92c830531425775a2006fa331097e11b32787a
RomanL91 <dev_roman91@mail.ru>
2025-08-12 02:43:41 +0500

    роутинг для auth

--- src/api/v1/ruotings.py
+++ src/api/v1/ruotings.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from api.v1.users.views import router as users_router
+from api.v1.auth.views import router as auth_router
 
 
 router = APIRouter()
 
-router.include_router(router=users_router, prefix="/auth")
+router.include_router(router=users_router, prefix="/users")
+router.include_router(router=auth_router, prefix="/auth")

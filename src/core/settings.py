cc0fc25b75e0f89bc920801b832d95f99a60b4d4
RomanL91 <dev_roman91@mail.ru>
2025-08-12 02:41:18 +0500

    настройки jwt

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,12 +1,15 @@
 import os
 
 from pathlib import Path
-from pydantic_settings import BaseSettings
 
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 
 from dotenv import load_dotenv
 
+
 load_dotenv()  # загрузка переменных окружения
+
 
 BASE_DIR = Path(__file__).parent.parent
 
@@ -25,6 +28,26 @@ class SettingsDataBase(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
+class SettingsAuth(BaseModel):
+    # пути к ключам по умолчанию: ./certs/private.pem, ./certs/public.pem
+    private_key_path: Path = BASE_DIR / "certs" / "private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "public.pem"
+
+    algorithm: str = os.getenv("JWT_ALG", "RS256")
+
+    token_type_field: str = os.getenv("JWT_TYPE_FIELD", "type")
+    token_type: str = os.getenv("JWT_TOKEN_TYPE", "Bearer")
+
+    access_token_type: str = os.getenv("JWT_ACCESS_TYPE", "access")
+    refresh_token_type: str = os.getenv("JWT_REFRESH_TYPE", "refresh")
+
+    # сроки жизни в минутах
+    access_token_expire: int = int(os.getenv("JWT_ACCESS_TTL_MIN", "15"))
+    refresh_token_expire: int = int(
+        os.getenv("JWT_REFRESH_TTL_MIN", str(60 * 24 * 14))
+    )  # 14 дней
+
+
 class Settings(BaseSettings):
     # == базовые настройки запуска сервиса
     SERVICE_HOST: str = os.getenv("SERVICE_HOST")
@@ -36,6 +59,9 @@ class Settings(BaseSettings):
 
     # == DataBase
     DATABASE: SettingsDataBase = SettingsDataBase()
+
+    # == JWT
+    AUTH_JWT: SettingsAuth = SettingsAuth()
 
 
 settings = Settings()

be386d3b2136098cde4a9ea57d8d61ecea9704d8
RomanL91 <dev_roman91@mail.ru>
2025-08-11 14:42:38 +0500

    первые конфигурации

---
+++ src/core/settings.py
@@ -0,0 +1,38 @@
+import os
+
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+
+from dotenv import load_dotenv
+
+load_dotenv()  # загрузка переменных окружения
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class SettingsDataBase(BaseSettings):
+    DB_NAME: str = os.getenv("POSTGRES_DB")
+    DB_USER: str = os.getenv("POSTGRES_USER")
+    DB_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    DB_HOST: str = os.getenv("POSTGRES_HOST")
+    DB_PORT: int = os.getenv("POSTGRES_PORT")
+
+    ECHO: int = os.getenv("ECHO")
+
+    @property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+class Settings(BaseSettings):
+    # == setting server running
+    SERVICE_HOST: str = os.getenv("SERVICE_HOST")
+    SERVICE_PORT: int = os.getenv("SERVICE_PORT")
+    SERVICE_RELOAD: int = os.getenv("SERVICE_RELOAD")
+
+    # == DataBase
+    db: SettingsDataBase = SettingsDataBase()
+
+
+settings = Settings()
